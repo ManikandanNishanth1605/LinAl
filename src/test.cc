8e3e31e14c2e9958c0956a5129f6f99602d79ac2
Manikandan Nishanth <nishanthmanikandanoffl@gmail.com>
2026-05-10 02:06:56 +0530

    Updated comments

--- src/test.cc
+++ src/test.cc
@@ -6,22 +6,12 @@
 using namespace std;
 typedef Matrix2D<double> matD;
 
-
-void printMatrix(double* mat, int row, int col){
-    int index = 0;
-    for (int i = 0 ; i < row ; i++){
-        for (int j = 0 ; j < col ; j++){
-            index = j + i * col;
-            cout << mat[index] << " ";
-        }
-        cout << endl;
-    }
-}
-
 int main(){
+    // Random number generator
     std::mt19937 gen(std::random_device{}());
     std::uniform_int_distribution<> dis(1, 10);
 
+    // Matrix array initialization
     int row1 = 3; int col1 = 3;
     int row2 = 3; int col2 = 3;
     int size1 = row1 * col1;
@@ -29,6 +19,7 @@ int main(){
     double* mat1 = new double[size1];
     double* mat2 = new double[size2];
     
+    // Assigning values
     for (int i = 0 ; i < size1 ; i++){
         mat1[i] = dis(gen)/10.0;
     }
@@ -36,19 +27,21 @@ int main(){
         mat2[i] = dis(gen)/10.0;
     }
 
+    // Matrix2D class instances
     matD matrix1(row1, col1, mat1);
-    matD matrix2(row2, col2, mat2);
-    matD matrix3 = matrix1 * matrix2;
-    
-    
-    // Log
     cout << "Matrix 1" << endl;
     cout << matrix1 << endl;
+    
+    matD matrix2(row2, col2, mat2);
     cout << "Matrix 2" << endl;
     cout << matrix2 << endl;
+    
+    // Matrix multiplication
+    matD matrix3 = matrix1 * matrix2;
     cout << "Matrix 3" << endl;
     cout << matrix3 << endl;
     
+    // Scalar multiplication
     double scalar = 10.0;
     matrix3 = scalar*matrix3;
     cout << "Matrix 3 after left scalar multiplication" << endl;
@@ -58,8 +51,9 @@ int main(){
     cout << "Matrix 2 after right scalar multiplication" << endl;
     cout << matrix2 << endl;
     
-    Matrix2D<double> matrix4 = matrix1 + matrix2;
-    cout << "Matrix 4" << endl;
+    // Matrix addition
+    matD matrix4 = matrix1 + matrix2;
+    cout << "Matrix 4 is matrix addition of Matrix 1 and 2" << endl;
     cout << matrix4 << endl;
 
     // Free memory
